e9213c900c227fb5876e5f248b2d65d65bcb3456
H <50N40W@users.noreply.github.com>
2020-09-23 18:43:45 -0400

    Update loop_control.java
    Changes for robotics practice on 24 Sep 2020

--- loop_control.java
+++ loop_control.java
@@ -1,4 +1,4 @@
-
+import java.util.concurrent.ThreadLocalRandom;
 /**
  * Write a description of class loop_control here.
  *
@@ -15,11 +15,19 @@ public class loop_control
     private static long SENSOR_PERIOD = 25; // milliseconds
     private static long NAV_PERIOD = 25;
     private static long ACTUATOR_PERIOD = 25;
+    private static long TELEMETRY_PERIOD = 200;
     
     private static int sensor_ctr = 0;
     private static int nav_ctr = 0;
     private static int actuator_ctr = 0;
-
+ 
+    public double pretend_stick_input(double in_radians) {
+        double stick_position = Math.sin(in_radians);
+        // question, what does that (double) statement do?
+        //stick_position = stick_position + Math.random()/2 - Math.random()/2;
+        System.out.println(stick_position);
+        return stick_position;
+    }
 
     public int loop_control()
     {
@@ -37,15 +45,20 @@ public class loop_control
         }
         if (millis - prev_nav_millis > NAV_PERIOD) 
         {
+            int j = 5;
             prev_nav_millis = millis;
             nav_ctr++;
             System.out.print("  navig.");
             System.out.print(nav_ctr);
             System.out.print("\t");
             System.out.println(millis);
+            j++;
+            System.out.println(j);
         }
+        //j = j /2;
         if (millis - prev_actuator_millis >= ACTUATOR_PERIOD) 
         {
+            
             prev_actuator_millis = millis;
             actuator_ctr++;
             System.out.print("     actuat.");
@@ -53,6 +66,15 @@ public class loop_control
             System.out.print("\t");
             System.out.println(millis);
         }
+        // if (millis - prev_telemetry_millis >= TELEMETRY_PERIOD) 
+        // {
+            // prev_actuator_millis = millis;
+            // actuator_ctr++;
+            // System.out.print("     actuat.");
+            // System.out.print(actuator_ctr);
+            // System.out.print("\t");
+            // System.out.println(millis);
+        // }
         // initialise instance variables
         return 0;
     }
@@ -65,10 +87,13 @@ public class loop_control
      */
     public int main_stub()
     {
+        double radians;
         // put your code here
-        int i = 0;
-        for (i = 0; i <= 10000000; i++) {
-            int loop_passed = loop_control();
+        //int i = 0;
+        //for (i = 0; i <= 300; i++) {
+        for (radians = 0; radians <= 2*Math.PI; radians = radians + Math.PI/60) {
+            //int loop_passed = loop_control();
+            double stick = pretend_stick_input(radians);
         }
         return 0;
     }
