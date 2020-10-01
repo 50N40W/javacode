d763ffc4518cdc72b572430ec2b6d23ea4faeb5f
H <50N40W@users.noreply.github.com>
2020-10-01 17:25:28 -0400

    Update loop_control.java
    Update for software meeting 1 Oct 2020

--- loop_control.java
+++ loop_control.java
@@ -12,14 +12,48 @@ public class loop_control
     private static long prev_sensor_millis= 0;
     private static long prev_nav_millis= 0;
     private static long prev_actuator_millis= 0;
+    private static long prev_telemetry_millis = 0;
     private static long SENSOR_PERIOD = 25; // milliseconds
     private static long NAV_PERIOD = 25;
     private static long ACTUATOR_PERIOD = 25;
-    private static long TELEMETRY_PERIOD = 200;
-    
+    private static long TELEMETRY_PERIOD = 500;
+    private static long stateTimer = 0;
+    float leftMotor = 0;
+    float rightMotor = 0;
+    float liftMotor = 0;
+    float clampServo = 0;
     private static int sensor_ctr = 0;
     private static int nav_ctr = 0;
     private static int actuator_ctr = 0;
+    final int UP = 1;
+    final int DN = -1;
+    final int INIT = -1;
+    final int CLMP = 0; // clamp: uses clamping, power, timeLim
+    final int MOVE = 1; // move: compass, forward, crab, power, timeLim
+    final int LIFT = 2; // lifter: uses lifter, power, timeLim
+    final int WAIT = 3;  // wait: uses timeLim
+    int autoState = CLMP;
+    String[] strAction = {"CLMP","LIFT","MOVE","MOVE","CLMP","MOVE", "WAIT", "WAIT"};
+    int[] action =   {CLMP,  LIFT, MOVE,  MOVE,  CLMP,  MOVE,  WAIT, WAIT};
+    float[] clamp =   { 20,     0,    0,     0,   -10,     0,   0,     0};
+    float[] lifter = {  0,     UP,    0,     0,     0,     0,   0,     0};
+    float[] pwrLeft = { 0,     25,   40,    30,     0,    25,   0,     0};
+    float[] pwrRight = {0,    -25,  -40,    30,     0,    25,   0,     0};
+    long[] timeLim = {300,   2000,  900,   500,  1000,  2000, 500,    50};
+    
+    public double motor_stick_relation(double stick_position) {
+        //double motor_command = sp * sp * sp * sp * sp;
+        double motor_command = Math.pow(stick_position,5);
+        return motor_command;
+    }
+    
+    public double first_order_filter(double current, double previous, double fc) {
+        double filtered_value = (fc * previous) + ((1-fc)*current);
+        /*  -- formula: filtered value = (filter constant * previous filtered
+         *    value) + (1-filter constant * raw value)
+         */
+        return filtered_value;
+    }
  
     public double pretend_stick_input(double in_radians) {
         /* pretty much every signal in the world is going to act like
@@ -37,6 +71,7 @@ public class loop_control
          * should give us a random number between -0.08 and 0.08, 
          * that SHOULD be a useful amount of noise for experimentation.
          */
+        //stick_position = in_radians;
         if (in_radians < Math.PI) {
            stick_position = Math.sin(in_radians);
         }
@@ -55,35 +90,60 @@ public class loop_control
         stick_position = Math.max(stick_position, -1);
         /* here we have ensured that stick position will be 
          * between -1 and 1, inclusive */
-        System.out.println(stick_position);
+        //System.out.println(stick_position);
         return stick_position;
     }
 
+    
+    
+    
     public int loop_control()
     {
-        //long millis = System.currentTimeMillis() % 1000;
+        
+        // states for the NAV switch statement
+
         long millis = System.currentTimeMillis();
         if (millis - prev_sensor_millis >= SENSOR_PERIOD)
         {
             prev_sensor_millis = millis;
             sensor_ctr++;
-            System.out.print("sense.");
-            System.out.print(sensor_ctr);
-            System.out.print(" ");
-            System.out.println(millis);
+
             //
         }
         if (millis - prev_nav_millis > NAV_PERIOD) 
         {
+            stateTimer += NAV_PERIOD;
             int j = 5;
+            boolean stageComplete = false;
             prev_nav_millis = millis;
-            nav_ctr++;
-            System.out.print("  navig.");
-            System.out.print(nav_ctr);
-            System.out.print("\t");
-            System.out.println(millis);
-            j++;
-            System.out.println(j);
+            stageComplete = (stateTimer > timeLim[autoState]);
+
+            switch (action[autoState])
+            {   
+                case CLMP:  // operate the clamp
+                    clampServo = clamp[autoState];
+                    break;
+                case LIFT:
+                    liftMotor = lifter[autoState];
+                    break;
+                case MOVE:   // move the bot, drive it somewhere
+                    rightMotor = pwrRight[autoState];
+                    leftMotor = pwrLeft[autoState];
+                case WAIT:
+                    break;
+                default:
+                    break;
+            }
+            if (stageComplete)
+            {
+                stateTimer= 0;
+                if (autoState <=6) autoState++;
+                liftMotor = 0;
+                leftMotor = 0;
+                rightMotor = 0;
+                clampServo = 0;
+            }
+
         }
         //j = j /2;
         if (millis - prev_actuator_millis >= ACTUATOR_PERIOD) 
@@ -91,22 +151,18 @@ public class loop_control
             
             prev_actuator_millis = millis;
             actuator_ctr++;
-            System.out.print("     actuat.");
-            System.out.print(actuator_ctr);
-            System.out.print("\t");
-            System.out.println(millis);
         }
-        // if (millis - prev_telemetry_millis >= TELEMETRY_PERIOD) 
-        // {
-            // prev_actuator_millis = millis;
-            // actuator_ctr++;
-            // System.out.print("     actuat.");
-            // System.out.print(actuator_ctr);
-            // System.out.print("\t");
-            // System.out.println(millis);
-        // }
+        if (millis - prev_telemetry_millis >= TELEMETRY_PERIOD) 
+        {
+            prev_telemetry_millis = millis;
+
+            //System.out.println(action[autoState]);
+            System.out.print(strAction[autoState]);
+            System.out.printf("\tLeft: %f,   Right:   %f \n", leftMotor, rightMotor);
+            System.out.printf("\tLifter: %f, Clamper: %f\n", liftMotor, clampServo);
+        }
         // initialise instance variables
-        return 0;
+        return autoState;
     }
 
     /**
@@ -115,16 +171,33 @@ public class loop_control
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int main_stub()
+    public void main_stub()
     {
-        double radians;
+        double radians = -1;
+        double prev_stick = 0;
+        final double FILT_CONST = 0.5;
+        final double FIRST_FILT = 0;
+        double fc = FIRST_FILT;
+
         // put your code here
         //int i = 0;
         //for (i = 0; i <= 300; i++) {
-        for (radians = 0; radians <= 3*Math.PI; radians = radians + Math.PI/60) {
+        //for (radians = 0; radians <= 3*Math.PI; radians = radians + Math.PI/60) {
+        //for (radians = -1; radians < 1; radians = radians + .05){
+        //while(radians < 1) {
             //int loop_passed = loop_control();
+        while(action[autoState] != WAIT) {
             double stick = pretend_stick_input(radians);
+            stick = first_order_filter(stick, prev_stick, fc);
+            prev_stick = stick;
+            fc = FILT_CONST;
+
+            double mtr_out = motor_stick_relation(stick);
+            int currentState = loop_control();
+
+            //System.out.printf("%f,%f, %f\n",radians, stick, mtr_out);
+            radians = radians + 0.05;
         }
-        return 0;
+
     }
 }
